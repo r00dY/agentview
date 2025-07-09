c6f83cc3de21b0ba5733e8089e40c09de3800100
Andrzej Dabrowski <a.r.dabrowski@gmail.com>
2025-07-09 15:02:31 +0200

    wip

--- frontend/app/lib/useFetcherSuccess.tsx
+++ frontend/app/lib/useFetcherSuccess.tsx
@@ -1,25 +1,43 @@
 import { useEffect, useRef } from "react";
-import type { FetcherWithComponents } from "react-router-dom";
+import type { FetcherWithComponents } from "react-router";
+import type { FormActionData, FormActionDataSuccess } from "./FormActionData";
 
-/**
- * Runs `cb` **once** every time a submission finishes successfully
- * (idle + some data, no error).
- */
-export function useFetcherSuccess<T>(
-  fetcher: FetcherWithComponents<T>,
-  cb: (data: T) => void
+export function useFetcherSuccess(
+  fetcher: FetcherWithComponents<FormActionData>,
+  cb: (data: any) => void
 ) {
   const prevState = useRef(fetcher.state);
 
   useEffect(() => {
     const justFinishedSuccessfully =
-      prevState.current === "loading" &&
+      (prevState.current === "loading" || prevState.current === "submitting") &&
       fetcher.state === "idle" &&
       fetcher.data &&
       fetcher.data.status === "success";
 
-    if (justFinishedSuccessfully) cb(fetcher.data.data as T);
+    if (justFinishedSuccessfully) {
+      cb((fetcher.data as FormActionDataSuccess).data);
+    }
 
     prevState.current = fetcher.state;
-  }, [fetcher.state, fetcher.data, fetcher.error, cb]);
+  }, [fetcher.state, fetcher.data, cb]);
 }
+
+// export function useFetcherSuccess<T>(
+//   fetcher: FetcherWithComponents<T>,
+//   cb: (data: T) => void
+// ) {
+//   const prevState = useRef(fetcher.state);
+
+//   useEffect(() => {
+//     const justFinishedSuccessfully =
+//       (prevState.current === "loading" || prevState.current === "submitting") &&
+//       fetcher.state === "idle" &&
+//       fetcher.data &&
+//       fetcher.data.status === "success";
+
+//     if (justFinishedSuccessfully) cb(fetcher.data.data as T);
+
+//     prevState.current = fetcher.state;
+//   }, [fetcher.state, fetcher.data, fetcher.error, cb]);
+// }
