32832abc702612ae04d0d6003062515960fd6a55
Andrzej Dabrowski <a.r.dabrowski@gmail.com>
2025-09-09 11:06:54 +0200

    wip

--- apps/studio/vite.config.ts
+++ apps/studio/vite.config.ts
@@ -6,7 +6,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
   server: {
-    port: 2137,
+    port: 1989,
   },
 });
 
