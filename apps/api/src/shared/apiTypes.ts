0c782cd85bb536dd33aef9b2cf6cefccca312c22
Andrzej Dabrowski <a.r.dabrowski@gmail.com>
2025-09-25 15:45:44 +0200

    wip

--- apps/api/src/shared/apiTypes.ts
+++ apps/api/src/shared/apiTypes.ts
@@ -25,7 +25,6 @@ export const InvitationCreateSchema = z.object({
     role: z.enum(['admin', 'user']),
 })
 
-
 export const VersionSchema = z.object({
     id: z.string(),
     version: z.string(),
