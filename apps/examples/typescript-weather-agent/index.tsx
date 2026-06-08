534acf2ad7e4d2d6a617a63f1afc95df4373b71d
Andrzej Dabrowski <a.r.dabrowski@gmail.com>
2026-06-08 12:51:18 +0200

    wip: remoed old projects

--- apps/examples/typescript-weather-agent/index.tsx
+++
@@ -1,8 +0,0 @@
-import "./styles.css";
-import { renderStudio } from "@agentview/studio";
-import agentviewConfig from "./agentview.config";
-
-renderStudio(
-    document.getElementById("agentview"), 
-    agentviewConfig
-);
