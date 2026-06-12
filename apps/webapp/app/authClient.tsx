abf14316212dd506f56f1ae34b63d70e6da4f535
Andrzej Dabrowski <a.r.dabrowski@gmail.com>
2026-06-12 12:52:43 +0200

    wip

--- apps/webapp/app/authClient.tsx
+++ apps/webapp/app/authClient.tsx
@@ -1,5 +1,6 @@
-import { apiKeyClient, organizationClient } from "better-auth/client/plugins"
+import { organizationClient } from "better-auth/client/plugins"
 import { createAuthClient } from "better-auth/react"
+import { apiKeyClient } from "@better-auth/api-key/client"
 
 export const authClient = createAuthClient({
     baseURL: import.meta.env.VITE_AGENTVIEW_API_URL + "/api/auth",
