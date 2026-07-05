835acc38d98152608bd196d817cf0d5b41d07c06
Andrzej Dabrowski <a.r.dabrowski@gmail.com>
2026-07-05 11:30:51 +0200

    wip

--- apps/examples/eve-in-next/.eve/dev-runtime/snapshots/mqp6ii8x-17c1febb-e723-49f4-9172-a4fb6314bf49/source/next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from "next";
-import { withEve } from "eve/next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default withEve(nextConfig);
